d5f8b5c1da6aa984d891875f283cb3258e151852
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-05-18 12:05:59 +1000

    Add image change on button hover

--- javascript.js
+++ javascript.js
@@ -1,38 +1,87 @@
-// CREDIT -- Text effect and Screen Overlay --
-// Dev:     Hyperplexed
-// Youtube: https://www.youtube.com/watch?v=jMVhxBB3l0w&ab_channel=Hyperplexed
-// CodePen: https://codepen.io/Hyperplexed/pen/vYzgeYE
-// Notes:   Modified to suit this project
-
+const playerCharacters = [];
+const aiCharacters = [];
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-const screen = document.querySelector(".screen");
-const nameSpan = document.querySelector(".name");
+const glitchButtons = document.querySelectorAll(".glitch-button");
 
-screen.onmouseenter = (e) => coolLetters(e);
+let stageOfPlay = 0; // entry into game
 
-function coolLetters() {
+function addButtonListeners() {
+	glitchButtons.forEach((button) => {
+		button.addEventListener("mouseenter", changeImage);
+	});
+}
+
+function changeImage(e) {
+	const characterName = e.target
+		.closest(".button-container")
+		.getAttribute("id");
+
+	let characterType;
+	let imageDiv;
+	let nameSpan;
+
+	//Check if character is player or ai
+	playerCharacters.forEach((character) => {
+		if (character.name === characterName) {
+			characterType = "player";
+		}
+	});
+
+	if (!characterType) {
+		characterType = "ai";
+	}
+	updateImage(characterName, characterType);
+}
+
+function updateImage(name, type) {
+	const screen = type === "player" ? "hero" : "opponent";
+	const imgPrefix = type === "player" ? "Hero_" : "AI_";
+	const imageDiv = document.querySelector(`.screen.${screen} .screen-image`);
+	const nameSpan = document.querySelector(`.screen.${screen} .name`);
+
+	imageDiv.style.backgroundImage = `url(/img/${imgPrefix}${name}.jpg)`;
+	updateScreenText(name, nameSpan);
+}
+
+function updateScreenText(name, nameSpan) {
+	console.log(name, nameSpan);
 	let interval = null;
 	let iteration = 0;
-
 	clearInterval(interval);
-
 	interval = setInterval(() => {
-		nameSpan.innerText = nameSpan.innerText
+		nameSpan.innerText = nameSpan.dataset.value
 			.split("")
 			.map((letter, index) => {
 				if (index < iteration) {
-					return nameSpan.dataset.value[index];
+					return name[index];
 				}
-
 				return letters[Math.floor(Math.random() * 26)];
 			})
 			.join("");
-
 		if (iteration >= nameSpan.dataset.value.length) {
 			clearInterval(interval);
 		}
-
 		iteration += 1 / 3;
 	}, 30);
 }
+
+function addCharacters() {
+	const playerNames = ["Byte", "Titan", "Claw", "Ace"];
+	const aiNames = ["Nexus", "Cipher", "Omega"];
+
+	playerNames.forEach((name) =>
+		playerCharacters.push(newCharacter(name, "player"))
+	);
+	aiNames.forEach((name) => aiCharacters.push(newCharacter(name, "ai")));
+}
+
+const newCharacter = (name, type) => {
+	// Ensure player image is in the format "Hero_Name.jpg"
+	const img = `/img/Hero_${name}.jpg`;
+	return { name, img };
+};
+
+addCharacters();
+
+addButtonListeners();
